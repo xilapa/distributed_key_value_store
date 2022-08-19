2f8d08075d1edeafa3482e9869207f6668e7cefb
Dirceu Junior <dirceu.sjr@gmail.com>
2022-08-19 07:45:19 -0300

    "Bucket Registry" client api

--- lib/kv/registry.ex
+++ lib/kv/registry.ex
@@ -1,11 +1,37 @@
 defmodule KV.Registry do
   use GenServer
 
-  # TODO: Client API
+  @type t() :: pid() | {atom(), node()} | atom() | {:global, any()} | {:via, module(), any()}
+
+  # Client API
+
+  @doc """
+  Starts the registry server.
+  """
+  @spec start_link([{atom(), any()}]) :: {:ok, pid()} | {:error, {:already_started, pid()}} | {:error, any()}
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  @doc """
+  Looks up the `bucket` `pid` for `name` in the `registry server`
+  """
+  @spec lookup(KV.Registry.t(), String.t()) :: {:ok, any()} | :error
+  def lookup(registry_server, name) do
+    GenServer.call(registry_server, {:lookup, name})
+  end
+
+  @doc """
+  Ensure the `bucket` with the given `name` is created on `registry_server`
+  """
+  @spec create(KV.Registry.t(), String.t()) :: :ok
+  def create(registry_server, name) do
+    GenServer.cast(registry_server, {:create, name})
+  end
 
   # GenServer Callbacks
   @impl GenServer
-  def init(_initial_state) do
+  def init(:ok) do
     {:ok, %{}}
   end
 
