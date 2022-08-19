2a1a68575ec8f009a06841cf27d3c26c02b7272c
Dirceu Junior <dirceu.sjr@gmail.com>
2022-08-19 17:50:44 -0300

    remove bucket from registry when it crash/end

--- lib/kv/registry.ex
+++ lib/kv/registry.ex
@@ -32,21 +32,41 @@ defmodule KV.Registry do
   # GenServer Callbacks
   @impl GenServer
   def init(:ok) do
-    {:ok, %{}}
+    names = %{} # correlates bucket names and pids
+    refs = %{} # correlates refs and bucket names
+    {:ok, {names, refs}}
   end
 
   @impl GenServer
-  def handle_call({:lookup, name}, _from, names) do
-    {:reply, Map.fetch(names, name), names}
+  def handle_call({:lookup, name}, _from, state) do
+    {names, _} = state
+    {:reply, Map.fetch(names, name), state}
   end
 
   @impl GenServer
-  def handle_cast({:create, name}, names) do
+  def handle_cast({:create, name}, {names, refs}) do
     if Map.has_key?(names, name) do
-      {:noreply, names}
+      {:noreply, {names, refs}}
     else
       {:ok, bucket} = KV.Bucket.start_link([])
-      {:noreply, Map.put(names, name, bucket)}
+      ref = Process.monitor(bucket)
+      refs = Map.put(refs, ref, name)
+      names = Map.put(names, name, bucket)
+      {:noreply, {names, refs}}
     end
+  end
+
+  @impl GenServer
+  def handle_info({:DOWN, ref, :process, _pid, _reason}, {names, refs}) do
+    {name, refs} = Map.pop(refs, ref)
+    names = Map.delete(names, name)
+    {:noreply, {names, refs}}
+  end
+
+  @impl GenServer
+  def handle_info(msg, state) do
+    require Logger
+    Logger.debug("Unexpected message on Registry: #{inspect(msg)}")
+    {:noreply, state}
   end
 end
