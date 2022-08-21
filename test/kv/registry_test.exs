3df380261ec29dd39ca3983e546889b1fae90ca4
Dirceu Junior <dirceu.sjr@gmail.com>
2022-08-21 13:07:35 -0300

    "Bucket Registry" shoul not crash if Bucket crashes

--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -26,4 +26,12 @@ defmodule KV.RegistryTest do
     Agent.stop(bucket)
     assert KV.Registry.lookup(registry, @shopping) == :error
   end
+
+  test "removes buckets on crash", %{registry: registry} do
+    KV.Registry.create(registry, @shopping)
+    assert {:ok, bucket} = KV.Registry.lookup(registry, @shopping)
+
+    Agent.stop(bucket, :shutdown)
+    assert KV.Registry.lookup(registry, @shopping) == :error
+  end
 end
