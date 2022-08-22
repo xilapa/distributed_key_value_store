6631cfcfd7c6418e1ef679717dd49138a6b7b93f
Dirceu Junior <dirceu.sjr@gmail.com>
2022-08-22 20:20:20 -0300

    listen to tcp port and spawn supervised tasks to handle requests

--- apps/kv_server/lib/kv_server.ex
+++ apps/kv_server/lib/kv_server.ex
@@ -1,18 +1,43 @@
 defmodule KVServer do
-  @moduledoc """
-  Documentation for `KVServer`.
-  """
+  require Logger
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> KVServer.hello()
-      :world
-
+  Starts listening connections on the given `port`.
   """
-  def hello do
-    :world
+  def listen(port) do
+    {:ok, socket} = :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
+    Logger.info("Accepting connections on port #{port}")
+    accept_connection_loop(socket)
+  end
+
+  defp accept_connection_loop(socket) do
+    {:ok, connected_socket} = :gen_tcp.accept(socket)
+    # aceita a conexão e dispara uma task para executar tal conexão
+    {:ok, pid} = Task.Supervisor.start_child(KVServer.TaskSupervisor, fn -> serve(connected_socket) end)
+
+    # This makes the child process the “controlling process” of the client socket.
+    #If we didn’t do this, the acceptor would bring down all the clients if it crashed
+    # because sockets would be tied to the process that accepted them
+    # (which is the default behaviour).
+    :ok = :gen_tcp.controlling_process(connected_socket, pid)
+
+    accept_connection_loop(socket)
+  end
+
+  defp serve(socket) do
+    socket
+    |> read_line()
+    |> write_line(socket)
+
+    serve(socket)
+  end
+
+  defp read_line(socket) do
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    data
+  end
+
+  defp write_line(data, socket) do
+    :gen_tcp.send(socket, data)
   end
 end
--- apps/kv_server/lib/kv_server/application.ex
+++ apps/kv_server/lib/kv_server/application.ex
@@ -6,10 +6,14 @@ defmodule KVServer.Application do
   use Application
 
   @impl true
+  @spec start(any, any) :: {:error, any} | {:ok, pid}
   def start(_type, _args) do
+    port = String.to_integer(System.get_env("PORT") || "4040")
     children = [
       # Starts a worker by calling: KVServer.Worker.start_link(arg)
       # {KVServer.Worker, arg}
+      {Task.Supervisor, name: KVServer.TaskSupervisor},
+      Supervisor.child_spec({Task, fn -> KVServer.listen(port) end}, restart: :permanent)
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
