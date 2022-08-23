6d1172e78a1164d6d56a121a1562dab07497b768
Dirceu Junior <dirceu.sjr@gmail.com>
2022-08-23 12:44:12 -0300

    added kv to extra applications at kv_server

--- apps/kv_server/mix.exs
+++ apps/kv_server/mix.exs
@@ -18,7 +18,7 @@ defmodule KVServer.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :kv],
       mod: {KVServer.Application, []}
     ]
   end
