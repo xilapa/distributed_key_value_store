54b8f4efd4af95afdc2e1ad91a54a2b11482f36a
Dirceu Junior <dirceu.sjr@gmail.com>
2022-08-23 18:19:47 -0300

    using the router on kv_server

--- apps/kv_server/lib/kv_server/command.ex
+++ apps/kv_server/lib/kv_server/command.ex
@@ -44,8 +44,10 @@ defmodule KVServer.Command do
   def run(command)
 
   def run({:create, bucket}) do
-    KV.Registry.create(KV.Registry, bucket)
-    {:ok, "OK\r\n"}
+    case KV.Router.route(bucket, KV.Registry, :create, [KV.Registry, bucket]) do
+      pid when is_pid(pid) -> {:ok, "OK\r\n"}
+      _ -> {:error, "FAILED TO CREATE BUCKET"}
+    end
   end
 
   def run({:get, bucket, key}) do
@@ -67,8 +69,8 @@ defmodule KVServer.Command do
   end
 
   defp lookup(bucket, callback) do
-    case KV.Registry.lookup(KV.Registry, bucket) do
-      {:ok, bucket_pid} -> callback.(bucket_pid)
+    case KV.Router.route(bucket, KV.Registry, :lookup, [KV.Registry, bucket]) do
+      {:ok, pid} -> callback.(pid)
       :error -> {:error, :not_found}
     end
   end
