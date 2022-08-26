8966d6c9b593265a6c03071d73756b9eef089809
Dirceu Junior <dirceu.sjr@gmail.com>
2022-08-25 21:38:22 -0300

    :kv removed from extra_applications of kv_server

--- apps/kv_server/mix.exs
+++ apps/kv_server/mix.exs
@@ -18,7 +18,7 @@ defmodule KVServer.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :kv],
+      extra_applications: [:logger],
       mod: {KVServer.Application, []}
     ]
   end
